6d66a37a244d9511261e78627e8ede64e231244d
mperestoronin <maxthirdmail@gmail.com>
2025-09-27 19:26:03 +0300

    added subject cards

--- src/data/lookups.js
+++ src/data/lookups.js
@@ -1,11 +1,112 @@
 export const backendURL = '/api/'
 
 
-// data/lookups.js
-export const faculties = ['Экономический', 'Филологический', 'Математический']
+export const faculties = [
+  { label: 'Экономический', value: 'eco' },
+  { label: 'Филологический', value: 'phil' },
+  { label: 'Математический', value: 'math' }
+]
 
-export const programs = ['Бакалавриат', 'Магистратура']
+export const programs = [
+  { label: 'Бакалавриат', value: 'bachelor' },
+  { label: 'Магистратура', value: 'master' }
+]
 
-export const courses = ['1 курс', '2 курс', '3 курс', '4 курс']
+export const courses = [
+  { label: '1 курс', value: '1' },
+  { label: '2 курс', value: '2' },
+  { label: '3 курс', value: '3' },
+  { label: '4 курс', value: '4' }
+]
 
-export const modules = ['Основы', 'Продвинутый уровень', 'Практика']
+export const modules = [
+  { label: 'Основы', value: 'base' },
+  { label: 'Продвинутый уровень', value: 'advanced' },
+  { label: 'Практика', value: 'practice' }
+]
+
+export const tags = [
+  'Экономика', 'Финансы', 'Бухгалтерия', 'История', 'Литература',
+  'Лингвистика', 'Математика', 'Алгебра', 'Геометрия', 'Статистика',
+  'Программирование', 'JavaScript', 'Python', 'Базы данных', 'Физика',
+  'Химия', 'Биология', 'Экология', 'Право', 'Менеджмент',
+  'Маркетинг', 'Логистика', 'Философия', 'Социология', 'Психология',
+  'Иностранные языки', 'Китайский', 'Английский', 'Немецкий', 'Французский'
+]
+// если тегов будет больше, можно смело подрезать .slice(0, 30) в компоненте
+
+// ----- список курсов -----
+export const coursesList = [
+  {
+    id: 'eco101',
+    title: 'Введение в экономику',
+    tags: ['Экономика', 'Финансы', 'История'],
+    faculty: 'eco',
+    program: 'bachelor',
+    course: '1',
+    module: 'base'
+  },
+  {
+    id: 'math201',
+    title: 'Линейная алгебра',
+    tags: ['Математика', 'Алгебра', 'Геометрия'],
+    faculty: 'math',
+    program: 'bachelor',
+    course: '2',
+    module: 'base'
+  },
+  {
+    id: 'phil301',
+    title: 'История философии',
+    tags: ['Философия', 'История', 'Социология'],
+    faculty: 'phil',
+    program: 'master',
+    course: '3',
+    module: 'advanced'
+  },
+  {
+    id: 'prog401',
+    title: 'Современное программирование',
+    tags: ['Программирование', 'Python', 'JavaScript'],
+    faculty: 'math',
+    program: 'master',
+    course: '4',
+    module: 'practice'
+  },
+    {
+    id: 'eco4101',
+    title: 'Введение в экономику',
+    tags: ['Экономика', 'Финансы', 'История'],
+    faculty: 'eco',
+    program: 'bachelor',
+    course: '1',
+    module: 'base'
+  },
+  {
+    id: 'math2401',
+    title: 'Линейная алгебра',
+    tags: ['Математика', 'Алгебра', 'Геометрия'],
+    faculty: 'math',
+    program: 'bachelor',
+    course: '2',
+    module: 'base'
+  },
+  {
+    id: 'phil4301',
+    title: 'История философии',
+    tags: ['Философия', 'История', 'Социология'],
+    faculty: 'phil',
+    program: 'master',
+    course: '3',
+    module: 'advanced'
+  },
+  {
+    id: 'prog4401',
+    title: 'Современное программирование',
+    tags: ['Программирование', 'Python', 'JavaScript'],
+    faculty: 'math',
+    program: 'master',
+    course: '4',
+    module: 'practice'
+  }
+]
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,75 @@
 <template>
-  <q-page class="flex flex-center">
-    <FiltersPanel
-  v-model="filters"
-  @apply="onApply"
-/>
+  <q-page class="q-pa-md">
+    <div class="row q-col-gutter-md">
+
+      <div class="col-12 col-md-3">
+        <FiltersPanel
+          v-model="filters"
+          @apply="onApply" 
+        />
+      </div>
+
+      <div class="col-12 col-md-6">
+        <CoursesList
+          v-model="selectedCourseIds"
+          :filters="filters"
+          height="calc(100vh - 320px)"
+          :search="searchQuery"
+          :activeTags="activeTags"
+          @reset="onReset"
+        />
+      </div>
+
+      <div class="col-12 col-md-3">
+        <q-card flat bordered>
+          <q-card-section class="text-subtitle2">Выбранные курсы</q-card-section>
+          <q-separator />
+          <q-list v-if="selectedCourseIds.length">
+            <q-item v-for="id in selectedCourseIds" :key="id">
+              <q-item-section>{{ id }}</q-item-section>
+            </q-item>
+          </q-list>
+          <div v-else class="q-pa-md text-grey">Ничего не выбрано</div>
+
+          <q-separator />
+          <q-card-actions align="right">
+            <q-btn label="Стр. 1" flat />
+            <q-btn label="Стр. 2" flat />
+            <q-btn label="Стр. 3" flat />
+          </q-card-actions>
+        </q-card>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import FiltersPanel from '../components/FiltersPanel.vue'
+import CoursesList from '../components/CoursesList.vue'
+
+const filters = ref({
+  faculty: null,
+  program: null,
+  course: null,
+  module: null
+})
+
+const selectedCourseIds = ref([])
+
+const searchQuery = ref('')
+const activeTags = ref([])
+
+
+function onApply (payload) {
+  console.log(payload)
+}
+
+function onReset () {
+  selectedCourseIds.value = []
+}
 </script>
+
+<style scoped>
+</style>
